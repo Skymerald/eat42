5f222262427c1b4045220baa2d8add8fa6308228
Skymerald <arthur.terrillon.sauvageot@gmail.com>
2024-12-03 16:50:32 +0100

    add shop.php to display the products and list_of_products.php to host the products in an array and create the corresponding object

---
+++ list_of_products.php
@@ -0,0 +1,11 @@
+<?php 
+include_once('products.php')
+$bowlBurger = ["name" => "Bowl Burger", "prix" => "70€", "description" => "Vous vous faites enflé certes mais c'est bon; en plus il y a du steak de la tomate et de la salade."];
+$tacosSavoyard = ["name" => "Tacos Savoyard", "prix" => "225€", "description" => "Une arnaque enore plus grosse mais aussi plus gourmande, accompagnée de ses frites surgelées, de petites galettes de pomme de terres et du bacon périmé."];
+$bowlCode42 = ["name" => "Bowl Code42", "prix" => "305€", "description" => "Qualtitatif, fait maison par code 42, expliquant donc le prix car ils n'ont pas le temps et bien sur accompagné d'oignions frit, de celeri, de poulet mariné, de frite, 
+de cornichons et de sauce algérienne, avec un assaisonnement sauce cancer. Cheh. Fallait pas gacher notre temps."];
+$saladeCesar = ["name" => "Salade César", "prix" => "185€", "description" => "Nettement plus pire que le bowl code42, mais bien meilleur que le bowl burger. Avec celui ci vous avez une chance de pas mourir dans la journée. 
+Mais attendez vous à mourir dans la semaine. Et ouais, fallait passez à la caisse, comme tout le monde, pour mourir dans 25 ans."];
+$tacosCaliente = ["name" => "Tacos Caliente", "prix" => "70€", "description" => "Va faire explosé votre colésthérol comme à trouer votre porte feuille. Sale pauvre. 
+Cela va aussi exploser vos toilettes grâce au piment jalapeno et habanero et la sauce samourai. Votre votre va aussi gonflé comme votre grand mère obèse grâce au steak, aux frites et aux tomates."];
+?>
